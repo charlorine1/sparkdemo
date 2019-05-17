dc493985834489554370cf3cda8db35f8858165c
yangchangxing <ycxusst@163.com>
2019-05-17 11:15:10 +0800

    1

--- src/main/java/com/usst/spark/other/CacheTest.java
+++ src/main/java/com/usst/spark/other/CacheTest.java
@@ -11,7 +11,7 @@ import org.apache.spark.storage.StorageLevel;
  *                     有3个patition ，内存个200M ，  如果内存是500M，则分布是：内存400M， 磁盘200M
  * 2、cache() persist() 对RDD持久化之后赋值给一个变量，那么下次使用到持久化的数据时，直接使用这个变量就是使用的持久化的数据据
  * 3、cache() persist() 后面不能紧跟action算子，因为action算子执行后的结果就不是rdd了，不是rdd就不能调用算子了
- * 4、persist的 _replication分区数？
+ * 4、    persist的 _replication分区数？
  * 
  * */
 public class CacheTest {
